1e8ee59819cc052e56d1502917dad3d0482613e8
shreyas5225 <shreyas3968@gmail.com>
2016-11-03 00:43:15 -0600

    timer functions

---
+++ DMA/Sources/timer.h
@@ -0,0 +1,16 @@
+/*
+ * timer.h
+ *
+ *  Created on: 02-Nov-2016
+ *      Author: Shreyas
+ */
+
+#ifndef SOURCES_TIMER_H_
+#define SOURCES_TIMER_H_
+
+void profiler_start();
+void profiler_stop();
+
+
+
+#endif /* SOURCES_TIMER_H_ */
