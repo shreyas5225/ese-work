f5d1e45a8e98706e99ae2341a6c11562ee0ec76b
Shreyas <Shreyas@DESKTOP-LDBA1TB>
2016-11-09 15:59:07 -0700

    timer change

--- DMA/Sources/timer.c
+++ DMA/Sources/timer.c
@@ -4,7 +4,7 @@
  *  Created on: 02-Nov-2016
  *      Author: Shreyas
  */
-/*
+
 #include "MKL25Z4.h"
 
 volatile uint32_t count = 0;
@@ -14,26 +14,26 @@ uint32_t b =0;
 void profiler_start()
 {
 				SIM->SOPT2 |= 0x01000000; /* use MCGFLLCLK as timer counter clock */
-				//SIM->SCGC6 |= 0x01000000; /* enable clock to TPM0 */
-				//TPM0->SC = 0; /* disable timer while configuring */
-				//TPM0->SC = 0x00; /* prescaler /1 */
-				//TPM0_MOD = 0x1E0;
+				SIM->SCGC6 |= 0x01000000; /* enable clock to TPM0 */
+				TPM0->SC = 0; /* disable timer while configuring */
+				TPM0->SC = 0x00; /* prescaler /1 */
+				TPM0_MOD = 0x1E0;
 
-				//TPM0->SC |= 0x80; /* clear TOF */
-				//TPM0_SC |= TPM_SC_TOIE_MASK;
-				//NVIC_EnableIRQ(TPM0_IRQn); /* enable IRQ17 (bit 17 of ISER[0]) */
+				TPM0->SC |= 0x80; /* clear TOF */
+				TPM0_SC |= TPM_SC_TOIE_MASK;
+				NVIC_EnableIRQ(TPM0_IRQn); /* enable IRQ17 (bit 17 of ISER[0]) */
 				//NVIC_EnableIRQ(DMA0_IRQn);
 
-				//TPM0->SC |= 0x40; /* enable timeout interrupt */
-				//TPM0->SC |= 0x08; /* enable timer */
+				TPM0->SC |= 0x40; /* enable timeout interrupt */
+				TPM0->SC |= 0x08; /* enable timer */
 
 
-//}
+}
 
-/*
+
 void profiler_stop()
 {
-	//NVIC_DisableIRQ(TPM0_IRQn);
+	NVIC_DisableIRQ(TPM0_IRQn);
 	//Disable the counter
 	TPM0_SC = 0;
 	TPM0_CONF = 0;
@@ -41,7 +41,7 @@ void profiler_stop()
 	b=a;
 }
 
-/*void TPM0_IRQHandler()
+void TPM0_IRQHandler()
 {
 	if(TPM0_SC & TPM_SC_TOF_MASK)
 	{
@@ -49,5 +49,5 @@ void profiler_stop()
 		count++;
 	}
 }
-*/
+
 
